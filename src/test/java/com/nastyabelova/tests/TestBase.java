7babe9e794969816cc5fbedf8e282489a1385014
Anastasiya Belova <anastasiya.v.belova@simbirsoft.com~>
2023-06-12 21:48:55 +0300

    Added test for jenkins report

--- src/test/java/com/nastyabelova/tests/TestBase.java
+++ src/test/java/com/nastyabelova/tests/TestBase.java
@@ -13,8 +13,8 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
     @BeforeEach
